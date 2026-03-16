8cee07c36133272e2e1ee342aff48ba1a6aa64d0
Oshan Thapa Chhetri <oshan9846320917@gmail.com>
2026-03-16 13:12:57 +1100

    change

--- assesment_1/regression.py
+++ assesment_1/regression.py
@@ -1,4 +1,7 @@
-import pandas as pd
+# Load the dataset
+data = pd.read_csv("../source/assessment_1_database_csvfile.csv")
 
-data = pd.read_csv('data.csv')
+# Show data
 print(data.head())
+print(data.info())
+print(data.describe())
